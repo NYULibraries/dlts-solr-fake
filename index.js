17a33c5e3065aaa0e393cb87b76a3793b78c4655
David <da70@nyu.edu>
2019-01-18 21:40:03 -0500

    First draft of plain Node http server version of Solr fake

--- index.js
+++ index.js
@@ -1,15 +1,75 @@
-const http = require( 'http' );
-const path = require( 'path' );
-const url  = require( 'url' );
+const http      = require( 'http' );
+const path      = require( 'path' );
+const url       = require( 'url' );
+
+const stringify = require( 'json-stable-stringify' );
 
 const DEFAULT_PORT = 3000;
 
+let solrResponses;
+
 function startSolrFake( solrResponsesIndex, solrResponsesDirectory, portArg ) {
-    const solrResponses = require( path.join( __dirname, 'database' ) )( solrResponsesIndex, solrResponsesDirectory );
+    solrResponses = getSolrResponses( solrResponsesIndex, solrResponsesDirectory );
 
     const port = portArg || DEFAULT_PORT;
 
-    console.log( 'JSON Server Solr fake is running on port ' + port );
+    http.createServer( handler ).listen( port )
+        .on( 'listening', () => {
+            console.log( 'Solr fake is running on port ' + port );
+        } )
+        .on( 'error', ( e ) => {
+            console.error( e );
+        } );
+}
+
+function getSolrResponses( solrResponsesIndex, solrResponsesDirectory ) {
+    const data = {};
+
+    const index = require( solrResponsesIndex );
+
+    Object.keys( index ).forEach( queryString => {
+        const file = path.join( solrResponsesDirectory, index[ queryString ] );
+
+        const response = require( file );
+
+        data[ normalizeQueryString( queryString ) ] = response;
+    } );
+
+    return data;
+}
+
+function normalizeQueryString( queryString ) {
+    queryString = decodeURI( queryString );
+
+    const urlSearchParams = new URLSearchParams( decodeURI( queryString ) );
+
+    urlSearchParams.sort();
+
+    return '?' + urlSearchParams.toString();
+}
+
+function handler( request, response ) {
+
+    const requestUrl = url.parse( request.url );
+
+    const normalizedQueryString = normalizeQueryString( requestUrl.search );
+
+    const solrResponse = solrResponses[ normalizedQueryString ] ?
+                         solrResponses[ normalizedQueryString ] :
+                         {
+                            error : `Query string "${ normalizedQueryString }" not found in index`
+                         };
+
+    const solrResponseString = stringify( solrResponse, { space: '    ' } );
+
+
+    response.writeHead( 200, {
+        "Content-Type"   : "text/plain;charset=utf-8",
+        "Content-Length" : solrResponseString.length,
+    } );
+
+    response.write( solrResponseString );
+    response.end();
 }
 
 module.exports.startSolrFake = startSolrFake;
