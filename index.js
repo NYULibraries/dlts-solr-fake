165f656c26945fa3f4eb8a8de928cab9c4acb3fa
David <da70@nyu.edu>
2019-01-21 14:07:16 -0500

    index.js: first draft of update solr response files feature

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const axios     = require( 'axios' );
+const crypto    = require( 'crypto' );
+const fs        = require( 'fs' );
 const http      = require( 'http' );
 const path      = require( 'path' );
 const url       = require( 'url' );
@@ -11,9 +14,30 @@ let solrResponsesIndex;
 let solrResponsesDirectory;
 let updateSolrResponsesSolrServerUrl;
 
+function getSolrResponseFilename( queryString ) {
+    const hash = crypto.createHmac( 'sha256', queryString )
+        .update( queryString )
+        .digest( 'hex' );
+
+    return `${ hash }.json`;
+}
+
 function getSolrResponseFilePath( responseFile ) {
     return path.join( solrResponsesDirectory, responseFile )
 }
+
+async function getSolrResponseFromLiveSolr( queryString ) {
+    try {
+        const request = updateSolrResponsesSolrServerUrl + queryString;
+
+        const response = await axios.get( request );
+
+        return response.data;
+    } catch( error ) {
+        console.error( error );
+    }
+}
+
 function getSolrResponses() {
     const data = {};
 
@@ -107,8 +131,49 @@ function startSolrFake(
         } );
 }
 
-function updateSolrResponsesHandler( request, response ) {
-    response.write( 'update!' );
+function stableStringify( data ) {
+    return stringify( data, { space : '    ' } );
+}
+
+function updateSolrResponses( queryString, solrResponse ) {
+    const index = require( solrResponsesIndex );
+
+    const responseFilename = getSolrResponseFilename( queryString );
+    const responseFilePath = getSolrResponseFilePath( responseFilename );
+
+    index[ queryString ] = responseFilename;
+
+    fs.writeFileSync( responseFilePath, solrResponse );
+
+    fs.writeFileSync( solrResponsesIndex, stableStringify( index ) );
+
+    console.log( 'Updated Solr responses: ' );
+    console.log( `"${ queryString } : ${ responseFilename }` );
+}
+
+async function updateSolrResponsesHandler( request, response ) {
+    const requestUrl = url.parse( request.url );
+
+    const queryString = requestUrl.search;
+
+    if ( ! queryString ) {
+        return;
+    }
+
+    const normalizedQueryString = normalizeQueryString( queryString );
+
+    let solrResponse = await getSolrResponseFromLiveSolr( normalizedQueryString );
+
+    const solrResponseString = stableStringify( solrResponse );
+
+    updateSolrResponses( normalizedQueryString, solrResponseString );
+
+    response.writeHead( 200, {
+        "Access-Control-Allow-Origin" : "*",
+        "Content-Type"                : "text/plain;charset=utf-8",
+    } );
+
+    response.write( solrResponseString );
     response.end();
 }
 
